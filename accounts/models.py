61f079a13c8a71aa75da02a8c4b807df75dcac07
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2024-02-27 21:50:25 +0100

    Add vendor-related files and models

--- accounts/models.py
+++ accounts/models.py
@@ -6,47 +6,44 @@ class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError("User must have an email address")
-        
+
         if not username:
             raise ValueError("User must have a username")
-        
+
         user = self.model(
-            email=self.normalize_email(email), 
-            username=username, 
-            first_name=first_name, 
-            last_name=last_name
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
-    
+
     def create_superuser(self, first_name, last_name, username, email, password=None):
         user = self.create_user(
-            email=self.normalize_email(email), 
-            username=username, 
-            first_name=first_name, 
-            last_name=last_name, 
-            password=password
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
         )
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
         user.is_superadmin = True
         user.save(using=self._db)
-        
+
         return user
 
 
 class User(AbstractBaseUser):
     RESTAURANT = 1
     CUSTOMER = 2
-    
-    ROLE_CHOICE = (
-        (RESTAURANT, 'Restaurant'),
-        (CUSTOMER, 'Customer')
-    )
-    
+
+    ROLE_CHOICE = ((RESTAURANT, "Restaurant"), (CUSTOMER, "Customer"))
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
@@ -61,26 +58,30 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-    
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
     objects = UserManager()
-    
+
     def __str__(self):
         return self.email
-    
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
-    
+
     def has_module_perms(self, add_label):
         return True
 
-    
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='users/profile_pictures/', null=True, blank=True)
-    cover_picture = models.ImageField(upload_to='users/cover_pictures/', null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="users/profile_pictures/", null=True, blank=True
+    )
+    cover_picture = models.ImageField(
+        upload_to="users/cover_pictures/", null=True, blank=True
+    )
     address_line_1 = models.CharField(max_length=100, null=True, blank=True)
     address_line_2 = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
@@ -90,7 +91,6 @@ class UserProfile(models.Model):
     longitude = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.user.email
-            
--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,75 @@
 from django.shortcuts import redirect, render
-from .models import User
+from vendor.forms import VendorForm
+from .models import User, UserProfile
 from .forms import UserForm
 from django.contrib import messages
 
+
 def registerUser(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
             user.role = User.CUSTOMER
             user.save()
-            messages.success(request, 'User registered successfully')
-            return redirect('registerUser')
+            messages.success(request, "User registered successfully")
+            return redirect("registerUser")
         else:
-            messages.error(request, 'User registration failed')
+            messages.error(request, "User registration failed")
     else:
         form = UserForm()
-        
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/registerUser.html', context)
+
+    context = {"form": form}
+    return render(request, "accounts/registerUser.html", context)
+
+
+def registerVendor(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        vendor_form = VendorForm(request.POST, request.FILES)
+
+        if form.is_valid() and vendor_form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+            user.role = User.RESTAURANT
+            user.save()
+
+            vendor = vendor_form.save(commit=False)
+            vendor.user = user
+            vendor.user_profile = UserProfile.objects.get(user=user)
+            vendor.save()
+
+            messages.success(
+                request,
+                "Your account has been registered succesfully, please wait for the approval.",
+            )
+            return redirect("registerVendor")
+        else:
+            messages.error(request, "Vendor registration failed")
+    else:
+        form = UserForm()
+        vendor_form = VendorForm()
+
+    context = {"form": form, "vendor_form": vendor_form}
+    return render(request, "accounts/registerVendor.html", context)
